10c7b6dfd6bb678988d375145649c6d500c00edd
ParasNn <paras04n@gmail.com>
2020-11-19 18:45:59 -0600

    finished Tele Op, (Servos, and motors for conveyor belt and shooter)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BoomerangTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BoomerangTeleOp.java
@@ -3,6 +3,7 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.CRServoImpl;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.util.ElapsedTime;
@@ -20,6 +21,22 @@ public class BoomerangTeleOp extends OpMode
     private boolean slowMode = false;
     private DcMotor roller = null;
     private boolean isIntakeRunning = false;
+    private DcMotor conveyorBeltL = null;
+    private DcMotor conveyorBeltR = null;
+    private DcMotor shooterMotor = null;
+    private CRServoImpl wobbleArmServo = null;
+    private CRServoImpl wobbleClawServo = null;
+    //Power variables
+    double leftFrontPower;
+    double rightFrontPower;
+    double leftBackPower;
+    double rightBackPower;
+    double maxPower;
+    double turn;
+    double conveyorPower = 0;
+    double shooterPower = 0;
+    double wobbleArmPower = 0;
+    double wobbleClawPower = 0;
 
     @Override
     public void init() {
@@ -31,14 +48,22 @@ public class BoomerangTeleOp extends OpMode
         leftBackDrive  = hardwareMap.get(DcMotor.class, "leftBack");
         rightBackDrive = hardwareMap.get(DcMotor.class, "rightBack");
         roller = hardwareMap.get(DcMotor.class, "roller");
+        conveyorBeltL = hardwareMap.get(DcMotor.class, "conveyorL");
+        conveyorBeltR = hardwareMap.get(DcMotor.class, "conveyorR");
+        shooterMotor = hardwareMap.get(DcMotor.class, "shooter");
+        wobbleArmServo = hardwareMap.get(CRServoImpl.class, "wobbleArm");
+        wobbleClawServo = hardwareMap.get(CRServoImpl.class, "wobbleClaw")
 
-
-        leftFrontDrive.setDirection(DcMotor.Direction.FORWARD);
+        leftFrontDrive.setDirection(DcMotor.Direction.REVERSE);
         rightFrontDrive.setDirection(DcMotor.Direction.FORWARD);
-        leftBackDrive.setDirection(DcMotor.Direction.FORWARD);
+        leftBackDrive.setDirection(DcMotor.Direction.REVERSE);
         rightBackDrive.setDirection(DcMotor.Direction.FORWARD);
         roller.setDirection(DcMotor.Direction.FORWARD);
-
+        conveyorBeltL.setDirection(DcMotor.Direction.FORWARD);
+        conveyorBeltR.setDirection(DcMotor.Direction.FORWARD);
+        shooterMotor.setDirection(DcMotor.Direction.FORWARD);
+        wobbleArmServo.setDirection(CRServoImpl.Direction.FORWARD);
+        wobbleClawServo.setDirection(CRServoImpl.Direction.FORWARD);
 
         telemetry.addData("Status", "Initialized");
         telemetry.update();
@@ -54,13 +79,6 @@ public class BoomerangTeleOp extends OpMode
 
     @Override
     public void loop() {
-
-        double leftFrontPower;
-        double rightFrontPower;
-        double leftBackPower;
-        double rightBackPower;
-        double maxPower;
-        double turn;
 
         turn = gamepad1.right_stick_x;
 
@@ -120,6 +138,42 @@ public class BoomerangTeleOp extends OpMode
         }
 
         roller.setPower(rollerPower);
+
+        if(gamepad2.right_bumper){
+            conveyorPower = 1;
+        }
+        else{
+            conveyorPower = 0;
+        }
+
+        conveyorBeltL.setPower(conveyorPower);
+        conveyorBeltR.setPower(conveyorPower);
+
+        if(gamepad2.right_trigger != 0){
+             shooterPower= 1;
+        }
+        else{
+            shooterPower = 0;
+        }
+
+        shooterMotor.setPower(shooterPower);
+
+        //Make an if statement to prevent the servo from going to far in certain directions.
+        wobbleArmPower = -gamepad2.left_stick_y;
+
+        //Same as before, add if statement to prevent servo angle from going too far.
+        if(gamepad2.x){
+            wobbleClawPower = 1;
+        }
+        else if(gamepad2.y){
+            wobbleClawPower = -1;
+        }
+        else{
+            wobbleClawPower = 0;
+        }
+
+        wobbleClawServo.setPower(wobbleClawPower);
+
 
         telemetry.addData("Status", "Run Time: " + runtime.toString());
         telemetry.update();
